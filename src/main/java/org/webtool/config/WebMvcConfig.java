f0fcee1ec078de3712d4cfe602edf83850e46fa5
LuciferLAPD <hoangmanh1505@gmail.com>
2019-06-14 06:57:34 +0700

    add theme

---
+++ src/main/java/org/webtool/config/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package org.webtool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurerAdapter
+{
+	
+	// Cấu hình để sử dụng các file nguồn tĩnh (html, image, ..)
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry)
+	{
+		registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/resource/css/")
+		        .setCachePeriod(31556926);
+		registry.addResourceHandler("/fonts/**").addResourceLocations("/WEB-INF/resource/fonts/")
+		        .setCachePeriod(31556926);
+		registry.addResourceHandler("/icon/**").addResourceLocations("/WEB-INF/resource/icon/")
+		        .setCachePeriod(31556926);
+		registry.addResourceHandler("/img/**").addResourceLocations("/WEB-INF/resource/img/")
+		        .setCachePeriod(31556926);
+		registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/resource/js/")
+		        .setCachePeriod(31556926);
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer)
+	{
+		configurer.enable();
+	}
+	
+}
